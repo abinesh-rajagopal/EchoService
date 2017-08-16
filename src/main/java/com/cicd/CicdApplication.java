808a8b3c3785eb9342862335ec952ea751cff4c4
Abinesh Rajagopal <AbineshRajagopal@clearchannel.com>
2017-08-16 12:42:26 -0400

    Added comment

--- src/main/java/com/cicd/CicdApplication.java
+++ src/main/java/com/cicd/CicdApplication.java
@@ -3,6 +3,7 @@ package com.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Starter class
 @SpringBootApplication
 public class CicdApplication {
 
